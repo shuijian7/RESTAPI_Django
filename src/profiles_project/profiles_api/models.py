43bbb98d5553c15af62eb5af5db4d645434624e8
shuijian <32333924+shuijian@users.noreply.github.com>
2019-03-10 01:11:23 -0800

    Enabled the Django admin

--- src/profiles_project/profiles_api/models.py
+++ src/profiles_project/profiles_api/models.py
@@ -14,7 +14,7 @@ class UserProfileManager(BaseUserManager):
             raise ValueError("Users must have an email address")
 
         email = self.normalize_email(email)
-        user = self.model(email = email,name=name)
+        user = self.model(email=email,name=name,)
 
         user.set_password(password)
         user.save(using=self._db)
@@ -42,7 +42,7 @@ class UserProfile(AbstractBaseUser,PermissionsMixin):
     objects = UserProfileManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELD = ['name']
+    REQUIRED_FIELDS = ['name']
 
     def get_full_name(self):
         """Used to get a users full name"""
