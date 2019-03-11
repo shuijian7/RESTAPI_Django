13eff5618bb90427766664e1c4e381c5436284ba
shuijian <32333924+shuijian@users.noreply.github.com>
2019-03-11 02:03:40 -0700

    API root,Profile,ProfileFeed

--- src/profiles_project/profiles_api/urls.py
+++ src/profiles_project/profiles_api/urls.py
@@ -1,7 +1,17 @@
 from django.conf.urls import url
+from django.conf.urls import include
+
+from rest_framework.routers import DefaultRouter
 
 from . import views
 
+router = DefaultRouter()
+router.register('hello-viewset',views.HelloViewSet,base_name = 'hello-viewset') #base name since not modelviewset
+router.register('profile', views.UserProfileViewSet) #not have base name since not modelviewset
+router.register('login', views.LoginViewSet,base_name = 'login') #base name since not modelviewset
+router.register('feed', views.UserProfileFeedViewSet)#not have base name since not modelviewset
 urlpatterns = [
     url(r'^hello-view/',views.HelloApiView.as_view()),
+    url(r'',include(router.urls))
+
 ]
